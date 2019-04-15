41cb6b31d885f27ec9113506be1ce125666fd732
dimasemin.98 <dimasemin.98@gmail.com>
2019-04-15 13:20:07 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        long pipkaMaxima = 2;
+
+        System.out.println("Pipka Maxima = " + pipkaMaxima + "cm");
+
+
+
+
+
+
+    }
+
+}
